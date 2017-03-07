83dfdf43011e192a3e122f3c7be34a6dfde2419d
Ward Scott <wscott@ncdot.gov>
2017-03-07 09:59:59 -0500

    basic scoring conditions done

--- app/modules/scoring.js
+++ app/modules/scoring.js
@@ -165,7 +165,6 @@ const endOfMatch = function(winningPlayer) {
   }
 }
 
-//I wasn't sure if we wanted to do a string with the final score or do an object literal in case we want to actually track this data at some point. went with the objec literal so it's less re-work later.
 const calculateFinalScore = function(loserSL, loserScore) {
 switch (loserSL) {
     case 1:
@@ -300,14 +299,12 @@ switch (loserSL) {
         }
         break;
     case 3:
-    //nested cases on individual score
     switch (loserScore) {
             case 0:
             case 1:
             case 2:
             case 3:
             case 4:
-            case 5:
                 return {
                     winnerScore: 20,
                     loserScore: 0
@@ -377,40 +374,620 @@ switch (loserSL) {
     break;
 
     case 4:
-    //nested cases on individual score
     switch (loserScore) {
-    }
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                return {
+                    winnerScore: 20,
+                    loserScore: 0
+                }
+                break;
+            case 6:
+            case 7:
+            case 8:
+                return {
+                    winnerScore: 19,
+                    loserScore: 1
+                }
+                break;
+            case 9:
+            case 10:
+            case 11:
+                return {
+                    winnerScore: 18,
+                    loserScore: 2
+                }
+                break;
+            case 12:
+            case 13:
+            case 14:
+                return {
+                    winnerScore: 17,
+                    loserScore: 3
+                }
+                break;
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+                return {
+                    winnerScore: 16,
+                    loserScore: 4
+                }
+                break;
+            case 19:
+            case 20:
+            case 21:
+                return {
+                    winnerScore: 15,
+                    loserScore: 5
+                }
+                break;
+            case 22:
+            case 23:
+            case 24:
+                return {
+                    winnerScore: 14,
+                    loserScore: 6
+                }
+                break;
+            case 25:
+            case 26:
+            case 27:
+                return {
+                    winnerScore: 13,
+                    loserScore: 7
+                }
+                break;
+            case 28:
+            case 29:
+            case 30:
+                return {
+                    winnerScore: 12,
+                    loserScore: 8
+                }
+                break;
+        }
     break;
+
     case 5:
-    //nested cases on individual score
     switch (loserScore) {
-    }
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                return {
+                    winnerScore: 20,
+                    loserScore: 0
+                }
+                break;
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+                return {
+                    winnerScore: 19,
+                    loserScore: 1
+                }
+                break;
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+                return {
+                    winnerScore: 18,
+                    loserScore: 2
+                }
+                break;
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+                return {
+                    winnerScore: 17,
+                    loserScore: 3
+                }
+                break;
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+                return {
+                    winnerScore: 16,
+                    loserScore: 4
+                }
+                break;
+            case 23:
+            case 24:
+            case 25:
+            case 26:
+                return {
+                    winnerScore: 15,
+                    loserScore: 5
+                }
+                break;
+            case 27:
+            case 28:
+            case 29:
+                return {
+                    winnerScore: 14,
+                    loserScore: 6
+                }
+                break;
+            case 30:
+            case 31:
+            case 32:
+            case 33:
+                return {
+                    winnerScore: 13,
+                    loserScore: 7
+                }
+                break;
+            case 34:
+            case 35:
+            case 36:
+            case 37:
+                return {
+                    winnerScore: 12,
+                    loserScore: 8
+                }
+                break;
+        }
     break;
+
     case 6:
     //nested cases on individual score
     switch (loserScore) {
-    }
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+                return {
+                    winnerScore: 20,
+                    loserScore: 0
+                }
+                break;
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+                return {
+                    winnerScore: 19,
+                    loserScore: 1
+                }
+                break;
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+                return {
+                    winnerScore: 18,
+                    loserScore: 2
+                }
+                break;
+            case 18:
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+                return {
+                    winnerScore: 17,
+                    loserScore: 3
+                }
+                break;
+            case 23:
+            case 24:
+            case 25:
+            case 26:
+            case 27:
+                return {
+                    winnerScore: 16,
+                    loserScore: 4
+                }
+                break;
+            case 28:
+            case 29:
+            case 30:
+            case 31:
+                return {
+                    winnerScore: 15,
+                    loserScore: 5
+                }
+                break;
+            case 32:
+            case 33:
+            case 34:
+            case 35:
+            case 36:
+                return {
+                    winnerScore: 14,
+                    loserScore: 6
+                }
+                break;
+            case 37:
+            case 38:
+            case 39:
+            case 40:
+                return {
+                    winnerScore: 13,
+                    loserScore: 7
+                }
+                break;
+            case 41:
+            case 42:
+            case 43:
+            case 44:
+            case 45:
+                return {
+                    winnerScore: 12,
+                    loserScore: 8
+                }
+                break;
+        }
     break;
+
     case 7:
-    //nested cases on individual score
+    
     switch (loserScore) {
-    }
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+                return {
+                    winnerScore: 20,
+                    loserScore: 0
+                }
+                break;
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+                return {
+                    winnerScore: 19,
+                    loserScore: 1
+                }
+                break;
+            case 16:
+            case 17:
+            case 18:
+            case 19:
+            case 20:
+            case 21:
+                return {
+                    winnerScore: 18,
+                    loserScore: 2
+                }
+                break;
+            case 22:
+            case 23:
+            case 24:
+            case 25:
+            case 26:
+                return {
+                    winnerScore: 17,
+                    loserScore: 3
+                }
+                break;
+            case 27:
+            case 28:
+            case 29:
+            case 30:
+            case 31:
+            case 32:
+                return {
+                    winnerScore: 16,
+                    loserScore: 4
+                }
+                break;
+            case 33:
+            case 34:
+            case 35:
+            case 36:
+            case 37:
+                return {
+                    winnerScore: 15,
+                    loserScore: 5
+                }
+                break;
+            case 38:
+            case 39:
+            case 40:
+            case 41:
+            case 42:
+            case 43:
+                return {
+                    winnerScore: 14,
+                    loserScore: 6
+                }
+                break;
+            case 44:
+            case 45:
+            case 46:
+            case 47:
+            case 48:
+            case 49:
+                return {
+                    winnerScore: 13,
+                    loserScore: 7
+                }
+                break;
+            case 50:
+            case 51:
+            case 52:
+            case 53:
+            case 54:
+                return {
+                    winnerScore: 12,
+                    loserScore: 8
+                }
+                break;
+        }
     break;
+
     case 8:
     //nested cases on individual score
     switch (loserScore) {
-    }
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+                return {
+                    winnerScore: 20,
+                    loserScore: 0
+                }
+                break;
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+            case 19:
+                return {
+                    winnerScore: 19,
+                    loserScore: 1
+                }
+                break;
+            case 21:
+            case 22:
+            case 23:
+            case 24:
+            case 25:
+            case 26:
+                return {
+                    winnerScore: 18,
+                    loserScore: 2
+                }
+                break;
+            case 27:
+            case 28:
+            case 29:
+            case 30:
+            case 31:
+            case 32:
+                return {
+                    winnerScore: 17,
+                    loserScore: 3
+                }
+                break;
+            case 33:
+            case 34:
+            case 35:
+            case 36:
+            case 37:
+            case 38:
+            case 39:
+                return {
+                    winnerScore: 16,
+                    loserScore: 4
+                }
+                break;
+            case 40:
+            case 41:
+            case 42:
+            case 43:
+            case 44:
+            case 45:
+                return {
+                    winnerScore: 15,
+                    loserScore: 5
+                }
+                break;
+            case 46:
+            case 47:
+            case 48:
+            case 49:
+            case 50:
+            case 51:
+            case 52:
+                return {
+                    winnerScore: 14,
+                    loserScore: 6
+                }
+                break;
+            case 53:
+            case 54:
+            case 55:
+            case 56:
+            case 57:
+            case 58:
+                return {
+                    winnerScore: 13,
+                    loserScore: 7
+                }
+                break;
+            case 59:
+            case 60:
+            case 61:
+            case 62:
+            case 63:
+            case 64:
+                return {
+                    winnerScore: 12,
+                    loserScore: 8
+                }
+                break;
+        }
     break;
+
     case 9:
     //nested cases on individual score
     switch (loserScore) {
-    }
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+                return {
+                    winnerScore: 20,
+                    loserScore: 0
+                }
+                break;
+            case 18:
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+            case 23:
+            case 24:
+                return {
+                    winnerScore: 19,
+                    loserScore: 1
+                }
+                break;
+            case 25:
+            case 26:
+            case 27:
+            case 28:
+            case 29:
+            case 30:
+            case 31:
+                return {
+                    winnerScore: 18,
+                    loserScore: 2
+                }
+                break;
+            case 32:
+            case 33:
+            case 34:
+            case 35:
+            case 36:
+            case 37:
+            case 38:
+                return {
+                    winnerScore: 17,
+                    loserScore: 3
+                }
+                break;
+            case 39:
+            case 40:
+            case 41:
+            case 42:
+            case 43:
+            case 44:
+            case 45:
+            case 46:
+                return {
+                    winnerScore: 16,
+                    loserScore: 4
+                }
+                break;
+            case 47:
+            case 48:
+            case 49:
+            case 50:
+            case 51:
+            case 52:
+            case 53:
+                return {
+                    winnerScore: 15,
+                    loserScore: 5
+                }
+                break;
+            case 54:
+            case 55:
+            case 56:
+            case 57:
+            case 58:
+            case 59:
+            case 60:
+                return {
+                    winnerScore: 14,
+                    loserScore: 6
+                }
+                break;
+            case 60:
+            case 61:
+            case 62:
+            case 63:
+            case 64:
+            case 65:
+            case 66:
+            case 67:
+                return {
+                    winnerScore: 13,
+                    loserScore: 7
+                }
+                break;
+            case 68:
+            case 69:
+            case 70:
+            case 71:
+            case 72:
+            case 73:
+            case 74:
+                return {
+                    winnerScore: 12,
+                    loserScore: 8
+                }
+                break;
+        }
     break;
-
-
 }
-
-
 }
 })();
 
